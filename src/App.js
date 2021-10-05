a2764b6a7d60caf662f5fb787e7181289a027922
xaviguasch <xaviguasch@gmail.com>
2021-10-05 22:34:47 +0200

    added basic styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Button from './components/Button'
 function App() {
   return (
     <div class='wrapper'>
+      <h1 class='title'>Buttons</h1>
       <h1>React!!!?!!!</h1>
       <Button></Button>
     </div>
